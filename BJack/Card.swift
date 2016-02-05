6082f2ead7c9c578760dfa5d40aacfff391f2c6b
Blake Elshire <belshire@jcp.com>
2016-02-05 16:16:41 -0600

    Basic card struct.

--- BJack/Card.swift
+++ BJack/Card.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct Card {
+    enum Suit: String {
+        case Spades = "Spades", Hearts = "Hearts", Diamonds = "Diamonds", Clubs = "Clubs"
+    }
+    
+    enum Rank: Int {
+        case Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+        case Jack, Queen, King, Ace
+        
+        struct Values {
+            let first: Int, second: Int?
+        }
+        
+        var values: Values {
+            switch self {
+            case .Ace:
+                return Values(first: 1, second: 11)
+            case .Jack, .Queen, .King:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+    
+    let rank: Rank, suit: Suit
+}
